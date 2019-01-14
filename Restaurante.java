d78859785ca46dd7fdeceb65a0f191b73a6c0b0e
Taysa Mendes <taysa_smp@hotmail.com>
2019-01-14 16:10:18 -0200

    Delete Restaurante.java

--- Restaurante.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Restaurante {
-	private ArrayList<Produto> produtos = new ArrayList<>();  
-	private ArrayList<Pedido> pedidos = new ArrayList<>();
-	private ArrayList<Cliente> clientes = new ArrayList<>();
-
-}
